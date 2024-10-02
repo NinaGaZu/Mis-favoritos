f8d5e1f2d8c53a9baadd974ec71a1cf3bd3b5139
Gianina Gaete <gianinagaete@gmail.com>
2024-10-02 19:07:48 -0300

    cards parte 1

--- assets/js/constants2.js
+++ assets/js/constants2.js
@@ -3,7 +3,7 @@ const cardData = [
   {
       imageSrc: '/assets/img/libros/juan-salvador-gaviota.jpg',
       name: 'Juan Salvador Gaviota',
-      description: 'Es una fábula sobre una gaviota que busca la perfección en el vuelo y descubre un sentido más profundo de la vida, más allá de las limitaciones de su bandada. La historia inspira a perseguir la libertad y la auto-superación. '
+      description: 'Es una fábula sobre una gaviota que busca la perfección en el vuelo y descubre un sentido más profundo de la vida, más allá de las limitaciones de su bandada. La historia inspira a perseguir la libertad y la auto-superación.'
   },
   {
       imageSrc: '/assets/img/libros/Orgullo-y-prejuicio.jpeg',
@@ -11,13 +11,23 @@ const cardData = [
       description: 'Narra la evolución de la relación entre Elizabeth Bennet y el señor Darcy, superando malentendidos y prejuicios en la Inglaterra rural del siglo XIX. Es una historia de amor y crítica social.'
   },
   {
-      imageSrc: 'img_avatar3.png',
-      name: 'Alice Johnson',
-      description: 'Loves to code'
+      imageSrc: '/assets/img/libros/Harry_Potter.jpeg',
+      name: 'Harry Potter',
+      description: 'Harry Potter, un joven mago, descubre su destino de derrotar al malvado Lord Voldemort mientras navega por la vida en Hogwarts, una escuela de magia. A lo largo de la saga, enfrenta desafíos, hace amigos leales y aprende valiosas lecciones sobre el amor y la valentía.'
   },
   {
-    imageSrc: 'img_avatar3.png',
-    name: 'Alice Johnson',
-    description: 'Loves to code'
+    imageSrc: '/assets/img/libros/la-odisea.jpg',
+    name: 'La Odisea',
+    description: 'La Odisea narra el largo y peligroso viaje de Ulises de regreso a Ítaca tras la guerra de Troya, enfrentando monstruos y dioses. Mientras tanto, su esposa Penélope lucha contra los pretendientes que quieren ocupar su lugar.'
+},
+{
+    imageSrc: '/assets/img/libros/Percy Jackson.jpeg',
+    name: 'Percy Jackson',
+    description: 'Percy Jackson, un joven semidiós hijo de Poseidón, descubre su verdadera identidad y se embarca en aventuras épicas para salvar al Olimpo de diversas amenazas. A lo largo de la saga, enfrenta monstruos mitológicos y dioses, mientras aprende sobre amistad y valentía.'
+},
+{
+    imageSrc: '/assets/img/libros/El juego de Ender.jpg',
+    name: 'El juego de Ender',
+    description: 'Sigue a Ender Wiggin, un niño prodigio que es entrenado por la Flota Internacional para liderar la defensa de la Tierra contra una invasión alienígena. A través de simulaciones y batallas, Ender debe superar desafíos estratégicos y morales para salvar a la humanidad.'
 }
 ];
--- assets/js/constants3.js
+++ assets/js/constants3.js
@@ -1,18 +1,73 @@
 // Datos de las Cards Peliculas-----para la imagen usar rutas absolutas desde raiz
 const cardData = [
   {
-      imageSrc: 'img_avatar1.png',
-      name: 'XXXHOLIC',
-      description: 'We love that guy'
+      imageSrc: '/assets/img/peliculas/Indiana Jones.jpeg',
+      name: 'Indiana Jones',
+      description: 'Indiana Jones, un arqueólogo aventurero, viaja por el mundo enfrentando trampas mortales y enemigos para recuperar artefactos históricos. Sus misiones lo llevan a descubrir tesoros perdidos y luchar contra villanos, como los nazis.'
   },
   {
-      imageSrc: 'img_avatar2.png',
-      name: 'Jane Smith',
-      description: 'Creativity is her passion'
+      imageSrc: '/assets/img/peliculas/El-Efecto-mariposa.jpg',
+      name: 'El Efecto Mariposa',
+      description: 'Evan Treborn descubre una técnica para viajar en el tiempo y reescribir su pasado traumático, con la esperanza de mejorar su presente. Sin embargo, cada cambio que realiza tiene consecuencias inesperadas y a menudo desastrosas.'
   },
   {
-      imageSrc: 'img_avatar3.png',
-      name: 'Alice Johnson',
-      description: 'Loves to code'
+      imageSrc: '/assets/img/peliculas/Juego de honor-coach Carter.jpeg',
+      name: 'Juego de Honor',
+      description: 'Sigue la historia real de Ken Carter, un entrenador de baloncesto que impone disciplina y valores académicos a su equipo de una escuela en Richmond, California. Su enfoque estricto transforma a los jugadores tanto en la cancha como en sus vidas personales.'
+  },
+  {
+      imageSrc: '/assets/img/peliculas/Escritores de la libertad.jpeg',
+      name: 'Escritores de la libertad',
+      description: 'Narra la historia real de Erin Gruwell, una maestra que inspira a sus estudiantes en riesgo a través de la escritura y la educación. Sus métodos transforman las vidas de los jóvenes, ayudándolos a superar prejuicios y encontrar su voz.'
+  },
+  {
+      imageSrc: '/assets/img/peliculas/Los suplentes.jpg',
+      name: 'Los Suplentes',
+      description: 'Los suplentes sigue a un equipo de fútbol americano que, durante una huelga de jugadores, contrata a un grupo de aficionados para competir en los play-offs. Estos jugadores inesperados deben superar sus diferencias y trabajar juntos para alcanzar el éxito.'
+  },
+  {
+      imageSrc: '/assets/img/peliculas/Curvas de la vida.jpg',
+      name: 'Curvas de la vida',
+      description: 'Gus, un veterano ojeador de béisbol que está perdiendo la vista, y su hija Mickey, mientras viajan juntos para evaluar a un joven talento. A lo largo del viaje, enfrentan conflictos personales y fortalecen su relación.'
+  },
+  {
+      imageSrc: '/assets/img/peliculas/Son como niños.jpg',
+      name: 'Son como niños',
+      description: 'Cinco amigos que se reúnen después de 30 años para asistir al funeral de su antiguo entrenador de baloncesto. Durante su reunión, redescubren la alegría de la niñez y viven divertidas situaciones juntos.'
+  },
+  {
+      imageSrc: '/assets/img/peliculas/El juego que hizo historia.jpeg',
+      name: 'El juego que hizo historia',
+      description: 'Narra la historia de Francis Ouimet, un joven golfista amateur de clase baja, que sueña con competir contra los mejores jugadores del mundo. A pesar de los obstáculos económicos y sociales, su talento y determinación lo llevan a participar en el U.S. Open de 1913.'
+  },
+  {
+      imageSrc: '/assets/img/peliculas/Duna.jpg',
+      name: 'Duna',
+      description: 'Paul Atreides, un joven noble debe viajar al peligroso planeta Arrakis para asegurar el futuro de su familia y su pueblo. Enfrentando traiciones y desafíos, Paul descubre su destino en medio de un conflicto por el control de la especia más valiosa del universo.'
+  },
+  {
+      imageSrc: '/assets/img/peliculas/El nombre de la rosa.jpeg',
+      name: 'El nombre de la rosa',
+      description: 'Guillermo de Baskerville, un monje franciscano, y su aprendiz Adso de Melk, mientras investigan una serie de asesinatos en una abadía benedictina. La historia explora la tensión entre la fe y la razón, y revela secretos oscuros en el corazón de la iglesia medieval.'
+  },
+  {
+      imageSrc: '/assets/img/peliculas/Constantine.jpeg',
+      name: 'Constantine',
+      description: 'John Constantine, un exorcista con la habilidad de ver ángeles y demonios en la Tierra, mientras lucha contra fuerzas sobrenaturales para salvar su alma. A lo largo de la película, enfrenta desafíos que ponen a prueba su fe y su determinación.'
+  },
+  {
+      imageSrc: '/assets/img/peliculas/Pitch perfect.jpg',
+      name: 'Pitch Perfect',
+      description: 'Pitch Perfect sigue a Beca, una estudiante universitaria que se une a un grupo de canto a capella y revoluciona su estilo con fusiones modernas. Juntas, las chicas compiten en concursos musicales universitarios, enfrentando desafíos y descubriendo su verdadero potencial.'
+  },
+  {
+      imageSrc: '/assets/img/peliculas/Assassins Creed.jpeg',
+      name: 'Assassin’s Creed',
+      description: 'Callum Lynch, revive las memorias de su ancestro Aguilar a través de una tecnología que accede a recuerdos genéticos. A medida que experimenta las aventuras de Aguilar, Callum descubre su conexión con una antigua orden de asesinos y lucha contra los templarios.'
+  },
+  {
+      imageSrc: '/assets/img/peliculas/Valiente.webp',
+      name: 'Valiente',
+      description: 'Una princesa escocesa llamada Mérida desafía las tradiciones y busca controlar su propio destino. Cuando un hechizo pone en peligro a su familia, debe usar su valentía y habilidades para salvarlos.'
   }
 ];
