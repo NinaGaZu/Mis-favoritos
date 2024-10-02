795e44bcb838c7bf8b78a40e16075b4733038782
Gianina Gaete <gianinagaete@gmail.com>
2024-10-01 22:17:48 -0300

    avance_01_10

--- assets/js/constants2.js
+++ assets/js/constants2.js
@@ -1,18 +1,23 @@
 // Datos de las Cards Libros-----para la imagen usar rutas absolutas desde raiz
 const cardData = [
   {
-      imageSrc: 'img_avatar1.png',
-      name: 'XXXHOLIC',
-      description: 'We love that guy'
+      imageSrc: '/assets/img/libros/juan-salvador-gaviota.jpg',
+      name: 'Juan Salvador Gaviota',
+      description: 'Es una fábula sobre una gaviota que busca la perfección en el vuelo y descubre un sentido más profundo de la vida, más allá de las limitaciones de su bandada. La historia inspira a perseguir la libertad y la auto-superación. '
   },
   {
-      imageSrc: 'img_avatar2.png',
-      name: 'Jane Smith',
-      description: 'Creativity is her passion'
+      imageSrc: '/assets/img/libros/Orgullo-y-prejuicio.jpeg',
+      name: 'Orgullo y Prejuicio',
+      description: 'Narra la evolución de la relación entre Elizabeth Bennet y el señor Darcy, superando malentendidos y prejuicios en la Inglaterra rural del siglo XIX. Es una historia de amor y crítica social.'
   },
   {
       imageSrc: 'img_avatar3.png',
       name: 'Alice Johnson',
       description: 'Loves to code'
-  }
+  },
+  {
+    imageSrc: 'img_avatar3.png',
+    name: 'Alice Johnson',
+    description: 'Loves to code'
+}
 ];
