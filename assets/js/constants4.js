c02914441219d035a106c481bf649c05e04d77b4
Gianina Gaete <gianinagaete@gmail.com>
2024-10-03 21:05:43 -0300

    cards parte 2

--- assets/js/constants4.js
+++ assets/js/constants4.js
@@ -1,18 +1,43 @@
 // Datos de las Cards Series-----para la imagen usar rutas absolutas desde raiz
 const cardData = [
   {
-      imageSrc: 'img_avatar1.png',
-      name: 'XXXHOLIC',
-      description: 'We love that guy'
+      imageSrc: '/assets/img/series/JAG.jpeg',
+      name: 'J.A.G.',
+      description: 'Sigue a Harmon Rabb, un piloto naval que, tras un accidente aéreo, es reasignado como abogado en el departamento de Judge Advocate General. Junto a su equipo, investiga y defiende casos de crímenes militares, enfrentando desafíos legales y personales.'
   },
   {
-      imageSrc: 'img_avatar2.png',
-      name: 'Jane Smith',
-      description: 'Creativity is her passion'
+      imageSrc: '/assets/img/series/Sweet Home.jpeg',
+      name: 'Sweet Home',
+      description: 'Cha Hyun-soo, un estudiante de secundaria que se muda a un nuevo apartamento tras la muerte de su familia. Allí, él y otros residentes deben enfrentarse a personas que se transforman en monstruos reflejando sus deseos más oscuros.'
   },
   {
-      imageSrc: 'img_avatar3.png',
-      name: 'Alice Johnson',
-      description: 'Loves to code'
+      imageSrc: '/assets/img/series/NCIS.jpeg',
+      name: 'NCIS',
+      description: 'Un equipo de agentes especiales del Servicio de Investigación Criminal Naval, liderado por Leroy Jethro Gibbs, que investiga crímenes relacionados con la Marina de los Estados Unidos. La serie combina drama, acción y procedimientos policiales mientras el equipo resuelve casos complejos y peligrosos.'
+  },
+  {
+      imageSrc: '/assets/img/series/Los magnificos.jpg',
+      name: 'Los Magníficos',
+      description: 'Grupo de cuatro ex-soldados que, tras ser encarcelados por un crimen que no cometieron, se fugan y se convierten en mercenarios que ayudan a los necesitados. A lo largo de la serie, enfrentan diversas aventuras y peligros mientras buscan justicia.'
+  },
+  {
+      imageSrc: '/assets/img/series/El reemplazante.jpeg',
+      name: 'El reemplazante',
+      description: 'Carlos Valdivia, un ex ejecutivo financiero que, tras perderlo todo, se convierte en profesor de matemáticas en un liceo marginal de Santiago. La serie aborda temas como la delincuencia juvenil y el embarazo adolescente.'
+  },
+  {
+      imageSrc: '/assets/img/series/Criminal minds.jpeg',
+      name: 'Criminal Minds',
+      description: 'Un equipo de perfiladores del FBI que analiza el comportamiento de criminales para anticipar sus próximos movimientos y capturarlos. La serie se centra en sus complejas investigaciones y los desafíos personales que enfrentan.'
+  },
+  {
+      imageSrc: '/assets/img/series/FBI.jpg',
+      name: 'FBI',
+      description: 'Sigue a un equipo de agentes especiales en la oficina de Nueva York del FBI, liderados por Maggie Bell y Omar Adom Zidan. Juntos, investigan casos de terrorismo, crimen organizado y contrainteligencia para proteger a la ciudad y al país.'
+  },
+  {
+      imageSrc: '/assets/img/series/Scorpion.jpg',
+      name: 'Scorpion',
+      description: 'Un grupo de genios liderados por Walter O’Brien, quienes forman una última línea de defensa contra amenazas globales complejas. Utilizan su intelecto para resolver problemas críticos y salvar vidas.'
   }
 ];
--- assets/js/constants5.js
+++ assets/js/constants5.js
@@ -1,17 +1,37 @@
 // Datos de las Cards Manga-----para la imagen usar rutas absolutas desde raiz
 const cardData = [
   {
-      imageSrc: 'img_avatar1.png',
-      name: 'XXXHOLIC',
-      description: 'We love that guy'
+      imageSrc: '/assets/img/comic/manga/Kobato.jpg',
+      name: 'Kobato',
+      description: 'Hanato Kobato, una joven optimista que debe sanar los corazones heridos de las personas para llenar una botella mágica y cumplir su deseo. Acompañada por su amigo de peluche Ioryogi, enfrenta diversos desafíos emocionales en su misión.'
   },
   {
-      imageSrc: 'img_avatar2.png',
+      imageSrc: '/assets/img/comic/manga/Lawful Drug.jpeg',
+      name: 'Lawful Drug',
+      description: 'Kazahaya Kudō, un joven con habilidades psíquicas trabaja en una farmacia junto a Rikuo Himura, quien también posee poderes sobrenaturales. Juntos, realizan misiones especiales para su misterioso jefe, Kakei.'
+  },
+  {
+      imageSrc: '/assets/img/comic/manga/Soul Eater.png',
+      name: 'Soul Eater',
+      description: 'Sigue a Maka Albarn y su compañero Soul Eater, quienes deben recolectar 99 almas corruptas y una de bruja para convertir a Soul en una Death Scythe. Juntos, enfrentan diversos enemigos y desafíos en la escuela Shibusen.'
+  },
+  {
+      imageSrc: '/assets/img/comic/manga/El chico que.webp',
+      name: 'El chico que le interesa no era un chico',
+      description: 'Historia entre una gyaru y su compañera de clase a quien confunde con un chico fuera de la escuela, uniéndose por su amor mutuo por el rock.'
+  },
+  {
+      imageSrc: '/assets/img/comic/manga',
+      name: 'Alice Johnson',
+      description: 'Loves to code'
+  },
+  {
+      imageSrc: '/assets/img/comic/manga',
       name: 'Jane Smith',
       description: 'Creativity is her passion'
   },
   {
-      imageSrc: 'img_avatar3.png',
+      imageSrc: '/assets/img/comic/manga',
       name: 'Alice Johnson',
       description: 'Loves to code'
   }
